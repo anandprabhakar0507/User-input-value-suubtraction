5964eb72b3aa3f0a535cafefacad01dabac12b8a
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-28 10:22:51 -0700

    Create Input diff.py

---
+++ Input diff.py
@@ -0,0 +1,12 @@
+#THIS SCRIPT TAKES INPUT OF NUMBERS and SUBTRACT THEM
+
+
+# Store input numbers
+A = input('')
+B = input('')
+print('YOU JUST ENTERED THE VALUE OF A={0} and B={1}'.format(A, B))
+# Subtract two numbers
+diff = float(A) - float(B)
+
+# Display the product
+print('The DIFFERENCE of A and B or A-B is {2}'.format(A, B, diff))
